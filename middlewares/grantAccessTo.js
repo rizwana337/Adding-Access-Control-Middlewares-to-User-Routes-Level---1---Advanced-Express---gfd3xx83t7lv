28071c313bd8299df268457ac671cfbd3d6f5481
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-24 22:24:38 +0530

    Update grantAccessTo.js

--- middlewares/grantAccessTo.js
+++ middlewares/grantAccessTo.js
@@ -8,17 +8,19 @@
         "message": "Access Denied"
     }
 */
-
 function grantAccessTo(roles) {
     try {
-        /*
-        Write your middleware here.
-        Steps: 
-        - Define a middleware function that takes the request, response, and next function as parameters.
-        - Extract the 'role' from req.body.
-        - Check if the required 'role' is present in the array.
-        - Return a valid response if access cannot be granted.
-        */
+        return function (req, res, next) {
+            const { role } = req.body;
+            if (!roles.includes(role)) {
+                return res.status(403).json({
+                    status: "error",
+                    message: "Access Denied"
+                })
+            } else {
+                next();
+            }
+        }
     } catch (err) {
         return res.status(400).json({
             status: "error",
@@ -27,5 +29,35 @@ function grantAccessTo(roles) {
     }
 }
 
-// Export the middleware function as a module.
-module.exports = { /*...*/ };
+module.exports = { grantAccessTo };
+// /*
+//     - Complete the Middleware grantAccessTo to manage access control for userRoutes. 
+//     - The middleware takes an array of roles as the parameter
+//     - Access should be granted to all roles in the parameter
+//     - Throw an error 403(Forbidden), if the role doesnt have access, in the given format: 
+//     {
+//         "status": "error",
+//         "message": "Access Denied"
+//     }
+// */
+
+// function grantAccessTo(roles) {
+//     try {
+//         /*
+//         Write your middleware here.
+//         Steps: 
+//         - Define a middleware function that takes the request, response, and next function as parameters.
+//         - Extract the 'role' from req.body.
+//         - Check if the required 'role' is present in the array.
+//         - Return a valid response if access cannot be granted.
+//         */
+//     } catch (err) {
+//         return res.status(400).json({
+//             status: "error",
+//             message: "Unable to check access level"
+//         })
+//     }
+// }
+
+// // Export the middleware function as a module.
+// module.exports = { /*...*/ };
